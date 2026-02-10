565511291b9432fea2ffd758ccc366bafeaf3cc2
mca044 <nidhishchandran24@gmail.com>
2026-02-10 11:48:22 +0530

    R-stat lab  added

--- Sem2/R-stat/Day4-03-02-2026.R
+++ Sem2/R-stat/Day4-03-02-2026.R
@@ -19,4 +19,71 @@ var(marks)
 summary(marks)
 
 #write marks of any 10 student and find mean, median,standard deviation,variance,and summary
-marks <- c(45,65,56,65,80,56,60,55,68,80)
+
+nmarks <- c(45,65,56,65,80,56,60,55,68,80)
+
+marks_fr<-as.data.frame(nmarks)
+marks_fr
+mean(nmarks)
+median(nmarks)
+sd(nmarks)
+var(nmarks)
+summary(nmarks)
+
+
+
+duration <-faithful$eruptions
+duration
+mean(duration)
+
+#plotting
+#Barplot
+
+barplot(
+        marks,
+        main="Students Marks",
+        xlab="students",
+        ylab = "marks",
+        col = "white",
+        border = "red"
+        )
+
+#pieplot
+
+pie(
+  marks,
+  main = "Pie chart of students marks",
+  col = rainbow(length(marks))
+)
+
+
+#lineplot
+
+plot(marks,
+     type = 'o',
+     col='blue',
+     main = "Line plot of marks",
+     xlab = 'studens',
+     ylab = 'marks')
+
+#mean of grouped data
+#class:40-50,50-60,60-70,70-80,80-90
+# freq:4,6,10,7,5,3
+
+#class intervals
+
+lower<-c(40,50,60,70,80)
+upper<-c(50,60,70,80,90)
+
+#frequency
+
+freq <-c(4,6,10,7,5,3)
+
+#class midpoint
+
+midpoint<-(lower+upper)/2
+midpoint
+
+#mean of grouped date
+
+mean_gd<-sum((freq*midpoint)/sum(freq))
---
+++ Sem2/R-stat/Day5-10-02-26.R
@@ -0,0 +1,118 @@
+#Question-1
+
+#Create a numeric vector
+num <-c(10,12,15,18,20,22,25,30,35,40,45,100)
+
+#mean of the num
+mean(num)
+
+#median of the num
+median(num)
+
+#first and third quartile
+quantile(num,probs=c(0.25,0.75))
+
+#summary of the num
+summary(num)
+
+#Question-2 => Calculate variance and standard deviation
+
+x<-c(10,12,15,18,20,22,25,30,35,40,45,100)
+n<-length(x)
+x_mean<-mean(x)
+variance<-sum(((x-x_mean)^2)/(n-1))
+std_dev<-sqrt(variance)
+variance
+std_dev
+
+#Question-3 => for a given data set show using R output that the mean and median are not equal
+#Explain the reason brie
+
+m<-c(10,12,15,18,20,22,25,30,35,40,45,100)
+mean(m)
+median(m)
+#The mean and median are not equal because the data set 
+#contain an extreme value The mean is affected by the outliers but 
+#median is not
+#Hence the mean get pulled towards the larger value
+
+
+
+# Part B:Grouped Data
+
+#Question 4:
+
+
+lower<-c(0,10,20,30,40)
+upper<-c(10,20,30,40,50)
+f<-c(3,7,12,8,5)
+
+mid<-(lower+upper)/2
+mid
+
+#mean of grouped data
+
+mean_grouped<-sum(f*mid)/sum(f)
+mean_grouped
+
+#Question 4B:
+
+cbind(lower,upper,mid)
+
+#Question 5:Cumulative frequency
+
+cf <-cumsum(f)
+cf
+
+#Total frequency
+N<-sum(f)
+N
+N/2
+
+#Find the median class (20-30)
+
+#Median
+
+L<-20
+cf_prev<-10
+f_m<-12
+h<-10
+N<-sum(f)
+
+median_grouped<-L+((N/2-cf_prev)/f_m)*h
+median_grouped
+
+#Part C: Data Visualization
+
+#Question 6
+
+classes<-c("0-10","10-20","20-30","30-40","40-50")
+freq<-c(3,7,12,8,5)
+
+barplot(freq,
+         names.arg=classes,
+         main="Number of Students in each classes interval",
+         xlab="Class interval",
+         ylab="Number of students",
+         col="Skyblue")
+
+#Question 7
+
+sales<-c(120,90,60,150,80)
+labels<-c("A","B","C","D","E")
+
+pie(sales,labels = labels,main="Sales of five Products")
+
+#B:
+
+percent<-round(sales/sum(sales)*100,1)
+pie(
+    sales,
+    labels = paste(labels,percent,'%'),
+    main="Sales of five Products"
+    )
+
+#Question 8
+
+
+
